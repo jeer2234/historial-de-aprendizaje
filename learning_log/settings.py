6517a75c47f3eda9999875159f63566253bbb204
Javier Espinoza <jeer2234@gmail.com>
2020-12-21 02:50:07 -0400

    Added h1 tag

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,7 +135,11 @@ STATIC_URL = '/static/'
 # My settings
 LOGIN_URL = 'users:login'
 
+
+
 # Heroku settings.
-import django_heroku
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 django_heroku.settings(locals())
 
